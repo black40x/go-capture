f102ba6b5781fa68bf4319009ac53e5b4134779a
DmVsk <dm.vsk@yandex.ru>
2022-05-23 16:43:37 +0400

    Add icon

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
-import "github.com/black40x/go-capture/pkg/capture"
+import (
+	_ "embed"
+	"github.com/getlantern/systray"
+)
+
+//go:embed assets/cam_white.png
+var icon []byte
+
+func onReady() {
+	systray.SetIcon(icon)
+	systray.SetTitle("")
+	systray.SetTooltip("Screen video capture")
+	mQuit := systray.AddMenuItem("Quit", "")
+	mQuit.SetIcon(icon)
+
+	go func() {
+		for {
+			select {
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+			}
+		}
+	}()
+}
+
+func onExit() {
+	// clean up here
+}
 
 func main() {
-	rec := capture.GetDisplayRect()
-	capture.CaptureStart(func(pix uint8) {
-		// write pixels
-	}, capture.Options{rec.Width, rec.Height})
+	systray.Run(onReady, onExit)
+	/*
+		rec := capture.GetDisplayRect()
+		capture.CaptureStart(func(pix uint8) {
+			// write pixels
+		}, capture.Options{rec.Width, rec.Height})
+	*/
 }
